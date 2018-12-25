45a7b4c40dfb67f69b7e5382abf420a8f2077677
Lucas Hilgevoord <lucashilgevoordd@gmail.com>
2018-12-25 01:51:44 +0100

    Assets/Basic Movement
    Assets imported:
    -Ball
    -Racket
    -Dispenser

---
+++ RV-Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("right"))
+        {
+            movePlayer(3.0f);
+        }
+        if (Input.GetKey("left"))
+        {
+            movePlayer(-3.0f);
+        }
+    }
+
+    public void movePlayer(float movingSpeed)
+    {
+        transform.position += Vector3.right * movingSpeed * Time.deltaTime;
+    }
+}
